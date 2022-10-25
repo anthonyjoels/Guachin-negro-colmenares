179a91a7c187ef0b616368902d76e7e999b2e2df
anthonyjoels <anthonyjoels96@gmail.com>
2022-10-25 16:22:15 -0300

    se creo el repositorio para la primera entrega de javascript

---
+++ index.js
@@ -0,0 +1,67 @@
+
+function bienvenida (user){
+       alert("Bienvenido " + user)
+}
+let nombre = prompt("cual es tu nombre?")
+bienvenida(nombre)
+
+
+
+
+let suma = parseInt(prompt(nombre + " para acceder necesitas resolver una operacion 1. continuar 2. salir"))
+if (suma === 1) {
+    alert("excelente, en seguida se mostrara la operacion")
+    let operacion = parseInt(prompt("cual es el resultado de la suma de 2 + 2?"))
+    if (operacion === 4) { alert("Bienvenido al Sitio " + nombre) }
+    else {
+        alert("la respuesta ingresada no es valida, actualice la pagina en intente de nuevo")
+    }
+}
+
+else if (suma === 2) { alert("Esta bien " + nombre + " adios") }
+
+
+
+let fotos = parseInt(prompt ("elige la foto que quieres comprar: 1.guachin en la terraza - 2.guachin durmiendo 3. guachin cazando 4. negro durmiendo 5. negro cazando 6. negro jugando"))
+
+let compraTotal = 0
+
+let seguirComprando = true
+
+let decision
+
+while (seguirComprando === true ) {
+
+    if (fotos === 1) {compraTotal =  compraTotal + 100
+    }
+    
+    else  if (fotos === 2) {compraTotal =  compraTotal + 110
+    }
+    
+    else  if (fotos === 3) {compraTotal =  compraTotal + 120
+    }
+
+    else  if (fotos === 4) {compraTotal =  compraTotal + 115
+    }
+
+    else  if (fotos === 5) {compraTotal =  compraTotal + 125
+    }
+
+    else  if (fotos === 6) {compraTotal =  compraTotal + 135
+    }
+
+    else{ fotos = parseInt(prompt ("escoge una foto valida:1.guachin en la terraza - 2.guachin durmiendo 3. guachin cazando 4. negro durmiendo 5. negro cazando 6. negro jugando"))
+    continue}
+
+    
+
+
+decision= parseInt(prompt("Quieres seguir escogiendo Fotos? 1.si 2.no"))
+if (decision === 1 ){ fotos = parseInt(prompt ("elige la foto que quieres comprar: 1.guachin en la terraza - 2.guachin durmiendo 3. guachin cazando 4. negro durmiendo 5. negro cazando 6. negro jugando"))
+}
+else{
+    seguirComprando = false
+}
+}
+
+alert("el monto final de tu compra es" + (compraTotal))

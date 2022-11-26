4fb0f8e7feaba2245549872937f8f62da1468fd6
anthonyjoels <anthonyjoels96@gmail.com>
2022-11-25 21:40:40 -0300

    se agrego alerta

--- index.js
+++ index.js
@@ -12,36 +12,37 @@ class producto {
 }
 }
 
-const producto0 = new producto (0, "guachin terraza", 100, 1, "./imagenes/guachin terraza.jpg");
-const producto1 = new producto (1, "negro terraza", 110, 2, "./imagenes/negro terraza.jpg");
-const producto2 = new producto (2, "guachin cazando", 200, 3, "./imagenes/guachin cazando.jpg");
-const producto3 = new producto (3, "negro cazando", 210, 4,"./imagenes/negro cazando.jpg");
-const producto4 = new producto (4, "guachin jugando", 300, 5, "./imagenes/guachin jugando.jpg");
-const producto5 = new producto (5, "negro jugando", 310, 6, "./imagenes/negro jugando.jpg");
-const producto6 = new producto (6, "guachin durmiendo", 400, 7, "./imagenes/guachin durmiendo.jpg");
-const producto7 = new producto (7, "negro durmiendo", 410, 8, "./imagenes/negro durmiendo.jpg");
-const producto8 = new producto (8, "guachin y negro juntos", 420, 8, "./imagenes/guachin y negro.jpg");
+const producto0 = new producto (0, "Guachin en la terraza", 100, 1, "./imagenes/guachin terraza.jpg");
+const producto1 = new producto (1, "Berlioz en la terraza", 110, 2, "./imagenes/negro terraza.jpg");
+const producto2 = new producto (2, "Guachin cazando", 200, 3, "./imagenes/guachin cazando.jpg");
+const producto3 = new producto (3, "Berlioz cazando", 210, 4,"./imagenes/negro cazando.jpg");
+const producto4 = new producto (4, "Guachin jugando", 300, 5, "./imagenes/guachin jugando.jpg");
+const producto5 = new producto (5, "Berlioz jugando", 310, 6, "./imagenes/negro jugando.jpg");
+const producto6 = new producto (6, "Guachin durmiendo", 400, 7, "./imagenes/guachin durmiendo.jpg");
+const producto7 = new producto (7, "Berlioz durmiendo", 410, 8, "./imagenes/negro durmiendo.jpg");
+const producto8 = new producto (8, "Guachin y Berlioz", 420, 8, "./imagenes/guachin y negro.jpg");
 
 
 const productos = [producto0, producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8];
 
-
-const carrito= JSON.parse(localStorage.getItem("carrito")) ||[]
-
-
 const divProductos = document.getElementById("divProductos")
-
-
-productos.forEach(producto=>{
+productos.forEach((producto)=>{
     divProductos.innerHTML += `
     <div id="${producto.id}" class="card tarjeta col-sm-12 col-md-6 col-lg-4  " >
    
-    <div class = "card-body" >
-    <img src="${producto.imagen}" height="70%" class="card-img-top imagenTarjeta" >
-    <h4 class="card-title tituloTarjeta">${producto.nombre}</h4>
+    <div class = "card-body " >
+    <img src="${producto.imagen}" height="70%" class="card-img-top imagenTarjeta " >
+
+    <div class="d-grid gap-2 col-6 mx-auto">
+    <h4 class="card-title tituloTarjeta ">${producto.nombre}</h4>
+    </div>
     
-    <p class="card-text precioTarjeta center">$ ${producto.precio}</p>
-    
+
+    <div class="d-grid gap-2 col-6 mx-auto">
+    <p class="card-text precioTarjeta center ">$ ${producto.precio}</p>
+    </div>
+
+
     <div class="d-grid gap-2 col-6 mx-auto">
     <buttom id=${producto.id} class="btn btn-primary botonTarjeta" > AGREGAR</buttom>
     </div>
@@ -53,28 +54,41 @@ productos.forEach(producto=>{
 })
 
 
-const botonesAgregar = document.querySelectorAll(".btn-primary")
+
+
+
+const carrito= JSON.parse(localStorage.getItem("carrito")) ||[]
+
+const botonesAgregar = document.querySelectorAll(".botonTarjeta")
 
 botonesAgregar.forEach(boton=>{
     boton.onclick = () => {
         const productoSeleccionado = productos.find(prod=> prod.id===parseInt(boton.id))
 
         const productoCarrito = {...productoSeleccionado,cantidad:1}
+
         const indexCarrito = carrito.findIndex(prod=>prod.id === productoCarrito.id)
         if(indexCarrito === -1){
             carrito.push(productoCarrito)
-        } else {
-            carrito[indexCarrito].cantidad +=1
+            
+        } 
+        else {
+            carrito[indexCarrito].cantidad += 1
         }
         localStorage.setItem("carrito", JSON.stringify(carrito))
+        console.log(carrito)
     }
 })
 
-const botonFinalizar = document.querySelector ("#finalizar")
-botonFinalizar.onclick = () => {
+const botonComprar = document.getElementById("comprar")
+botonComprar.onclick = () => {
     const valores = carrito.map(prod=> prod.precio * prod.cantidad)
+    
     let totalCompra = 0
     valores.forEach(valor=>{
-        totalCompra += valor
-    })
+        totalCompra += valor})
+        console.log(totalCompra)
+        totalCompra.onclick = Swal.fire (`  El total de su compra es: ${totalCompra}`)
+        
+    
 }
